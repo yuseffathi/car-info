ce8b4648caf8215ed9d369cbf103135bba438d0c
yusef fathi <yuseffathi@gmail.com>
2025-01-30 14:33:02 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: {
-        filename: "static/js/[name].js",
-        chunkFilename: "static/js/[name].js",
-        assetModuleFilename: "static/media/[name][ext]",
-    },
+
 };
 
 export default nextConfig;
